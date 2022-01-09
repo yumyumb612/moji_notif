3f5af96170b571247176dcf35d9be358fb6774b7
kshly <yumyum_b612@protonmail.ch>
2022-01-08 23:41:24 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,16 +1,17 @@
-#   api can be:
-#   1. history - http://numbersapi.com/{current date}/date
-#   2. number_trivia - http://numbersapi.com/random/trivia
-#   3. cat_fact - https://cat-fact.herokuapp.com
-#   4. advice - https://api.adviceslip.com
-#   5. random - Mixure of all
+#   api can be: 
+#   - history (history of wat da present month and day)
+#   - number_trivia (number trivias)
+#   - cat_fact (fun facts about cats! meow cat)
+#   - advice ('useful' advices somtimes nonsense :3)
+#   - random
+
 api = ""
 
 
-#   icon_path specifies an icon filename and its path to display. put none is you want to diable it.
+#   icon_path specifies an icon filename and its path to display. put none if you want to diable it.
 icon_path = "~/.config/moji_notif/moji.png"
 
-#   ringtone_path specifies a music file to play upon sending a notification. put none is you want to diable it.
+#   ringtone_path specifies a music file to play upon sending a notification. put none if you want to diable it.
 ringtone_path = "~/.config/moji_notif/ting.mp3"
 
 #   time specifies the timeout in milliseconds at which to expire the notification.
