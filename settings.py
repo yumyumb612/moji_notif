815973cd25ef6e861cca6377437bbfbb4080bc9a
kshly <yumyum_b612@protonmail.ch>
2022-01-10 12:39:01 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
-#   api value can be: 
-#   history - history of wat happend dis present month and day
-#   number_trivia - random number trivias
-#   cat_fact - fun facts about cats! meow cat
-#   advice - "useful" advices somtimes nonsense :3
-#   random
-# api = "random"
+#   da value for api can be one of the followings: 
+#   * history - history of wat happend dis present month and day
+#   * number_trivia - random number trivias
+#   * cat_fact - fun facts about cats! meow cat
+#   * advice - "useful" advices somtimes nonsense :3
+#   * random
+#api = "random"
 api = ""
 
 
 #   icon_path specifies an icon filename and its path to display. put none if you want to diable it.
-icon_path = "~/.config/moji_notif/moji.png"
+icon_path = "~/.config/moji_notif/Assets/moji.png"
 
 #   ringtone_path specifies a music file to play upon sending a notification. put none if you want to diable it.
-ringtone_path = "~/.config/moji_notif/ting.mp3"
+ringtone_path = "~/.config/moji_notif/Assets/ting.mp3"
 
 #   time specifies the timeout in milliseconds at which to expire the notification.
 time = 60000
